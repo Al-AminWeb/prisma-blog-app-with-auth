c154bacae1738282ba37be00ea7745c5a8060e7b
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-01-09 21:38:18 +0600

    comment router setup and post analysis

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import {postRouter} from "./modules/post/post.router";
 import {toNodeHandler} from "better-auth/node";
 import {auth} from "./lib/auth";
 import cors from "cors";
+import {commentRouter} from "./modules/comment/comment.router";
 
 
 const app: Application = express()
@@ -15,6 +16,7 @@ app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use(express.json());
 
 app.use('/post', postRouter);
+app.use("/comments",commentRouter)
 
 app.get("/", (req, res) => {
     res.send("Hello, World!")
